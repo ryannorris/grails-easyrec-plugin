e18f9c08abc37c9ce3dbfe8ae01af35c5ca905c0
Ryan Norris <ryannorris@gmail.com>
2011-08-26 09:06:34 -0400

    Initial commit

---
+++ easyrec-plugin/grails-app/services/easyrec/plugin/EasyRecService.groovy
@@ -0,0 +1,31 @@
+package easyrec.plugin
+
+import groovyx.net.http.HTTPBuilder
+
+class EasyRecService {
+
+    static transactional = true
+
+    def view(String userId, String sessionId, String itemId, String itemDescription, String itemUrl, String itemImageUrl = null, String actionTime = null) {
+		String apiKey = '3532665f1995cc509449d9a8f0600660'
+		String tenantId = 'SWAPFISH'
+		
+		def http = new HTTPBuilder('http://intralife.researchstudio.at:8080')
+		
+		def response = http.get(path: '/api/1.0/view', query: [apikey: apiKey, tenantid: tenantId, userid: userId, sessionid: sessionId, itemid: itemId, itemdescription: itemDescription, itemurl: itemUrl, itemimageurl: itemImageUrl])
+		
+			println response
+		
+    }
+	
+	def recomendationsForUser(String userId) {
+		String apiKey = '3532665f1995cc509449d9a8f0600660'
+		String tenantId = 'SWAPFISH'
+		
+		def http = new HTTPBuilder('http://intralife.researchstudio.at:8080')
+		
+		def response = http.get(path: '/api/1.0/recommendationsforuser', query: [apikey: apiKey, tenantid: tenantId, userid: userId])
+		
+		return response
+	}
+}
---
+++ easyrec-plugin/test/unit/easyrec/plugin/EasyRecServiceTests.groovy
@@ -0,0 +1,17 @@
+package easyrec.plugin
+
+import grails.test.*
+
+class EasyRecServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
