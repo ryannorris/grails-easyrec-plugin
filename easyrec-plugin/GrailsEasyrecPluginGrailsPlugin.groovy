8ede681b836f6e6388f4a37dec449ce35e364c85
Ryan Norris <ryannorris@gmail.com>
2011-08-27 17:29:21 -0400

    Renaming stuff

--- easyrec-plugin/GrailsEasyrecPluginGrailsPlugin.groovy
+++ easyrec-plugin/GrailsEasyrecPluginGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class EasyrecPluginGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [rest:0.6.1]
+    def dependsOn = [rest:'0.6.1']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
