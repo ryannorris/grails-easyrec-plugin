763783a79e249b0a378619a92fe45d86442fe544
Ryan Norris <ryannorris@gmail.com>
2011-08-28 13:34:39 -0400

    Fixed return type

--- easyrec-plugin/grails-app/services/easyrec/plugin/EasyRecService.groovy
+++ easyrec-plugin/grails-app/services/easyrec/plugin/EasyRecService.groovy
@@ -1,32 +1,29 @@
 package easyrec.plugin
 
 import groovyx.net.http.HTTPBuilder;
-
-
 class EasyRecService {
 
-    static transactional = true
+	static transactional = true
 
-    def view(String userId, String sessionId, String itemId, String itemDescription, String itemUrl, String itemImageUrl = null, String actionTime = null) {
+	def view(String userId, String sessionId, String itemId, String itemDescription, String itemUrl, String itemImageUrl = null, String actionTime = null) {
 		String apiKey = '3532665f1995cc509449d9a8f0600660'
 		String tenantId = 'SWAPFISH'
-		
+
 		def http = new HTTPBuilder('http://intralife.researchstudio.at:8080')
-		
+
 		def response = http.get(path: '/api/1.0/view', query: [apikey: apiKey, tenantid: tenantId, userid: userId, sessionid: sessionId, itemid: itemId, itemdescription: itemDescription, itemurl: itemUrl, itemimageurl: itemImageUrl])
-		
-			println response
-		
-    }
-	
+
+		return response
+	}
+
 	def recomendationsForUser(String userId) {
 		String apiKey = '3532665f1995cc509449d9a8f0600660'
 		String tenantId = 'SWAPFISH'
-		
+
 		def http = new HTTPBuilder('http://intralife.researchstudio.at:8080')
-		
+
 		def response = http.get(path: '/api/1.0/recommendationsforuser', query: [apikey: apiKey, tenantid: tenantId, userid: userId])
-		
+
 		return response
 	}
 }
