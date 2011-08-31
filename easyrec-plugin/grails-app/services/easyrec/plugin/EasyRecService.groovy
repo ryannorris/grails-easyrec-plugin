a83f8e7516379f3551b4f17d91277f5e8520288f
Igor Artamonov <igor@artamonov.ru>
2011-08-30 21:40:12 -0400

    make it configurable

--- easyrec-plugin/grails-app/services/easyrec/plugin/EasyRecService.groovy
+++ easyrec-plugin/grails-app/services/easyrec/plugin/EasyRecService.groovy
@@ -1,29 +1,55 @@
 package easyrec.plugin
 
 import groovyx.net.http.HTTPBuilder;
-class EasyRecService {
+import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
+import org.springframework.beans.factory.InitializingBean
+import groovyx.net.http.HttpResponseException
 
+class EasyRecService implements InitializingBean {
 	static transactional = true
 
+    String apiKey
+    String tenantId
+    String url
+    String context
+
 	def view(String userId, String sessionId, String itemId, String itemDescription, String itemUrl, String itemImageUrl = null, String actionTime = null) {
-		String apiKey = '3532665f1995cc509449d9a8f0600660'
-		String tenantId = 'SWAPFISH'
+		def http = new HTTPBuilder(url)
 
-		def http = new HTTPBuilder('http://intralife.researchstudio.at:8080')
-
-		def response = http.get(path: '/api/1.0/view', query: [apikey: apiKey, tenantid: tenantId, userid: userId, sessionid: sessionId, itemid: itemId, itemdescription: itemDescription, itemurl: itemUrl, itemimageurl: itemImageUrl])
+		def response = http.get(path: getPath('/api/1.0/view'),
+                query: [apikey: apiKey, tenantid: tenantId, userid: userId,
+                        sessionid: sessionId, itemid: itemId, itemdescription: itemDescription, itemurl: itemUrl, itemimageurl: itemImageUrl])
 
 		return response
 	}
 
 	def recomendationsForUser(String userId) {
-		String apiKey = '3532665f1995cc509449d9a8f0600660'
-		String tenantId = 'SWAPFISH'
-
-		def http = new HTTPBuilder('http://intralife.researchstudio.at:8080')
-
-		def response = http.get(path: '/api/1.0/recommendationsforuser', query: [apikey: apiKey, tenantid: tenantId, userid: userId])
-
-		return response
+        try {
+            def http = new HTTPBuilder(url)
+            def response = http.get(path: getPath('/api/1.0/recommendationsforuser'),
+                    query: [apikey: apiKey, tenantid: tenantId, userid: userId])
+            return response
+        } catch (HttpResponseException e) {
+            log.error("Not found. By url: $url / $context", e)
+            throw e //TODO really?
+        }
 	}
+
+    String getPath(String path) {
+        StringBuilder buf =  new StringBuilder()
+        if (context && context.length() > 0) {
+            buf.append('/')
+            buf.append(context)
+        }
+        buf.append(path)
+        return buf
+    }
+
+    void afterPropertiesSet() {
+        def conf = CH.config.easyrec
+        apiKey = conf.key
+        tenantId = conf.tenantId
+        url = conf.url
+        context = conf.containsKey('urlContext') ? conf.urlContext : null
+    }
 }
